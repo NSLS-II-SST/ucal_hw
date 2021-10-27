5397d9af7362fbf27f86786042ba841a5f038f1c
Jamie Titus <ctitus@stanford.edu>
2021-10-27 14:17:57 -0400

    manipulator motor PVs added

--- ucal_hw/motors.py
+++ ucal_hw/motors.py
@@ -1,3 +1,11 @@
 from bl_base.motors import PrettyMotorFMBO
 
-# tesz = PrettyMotorFMBO("MCS8:TES:PLACEHOLDER", name = "TES z")
+# tesz = PrettyMotorFMBO("XF:07ID1-BI{UCAL-Ax:DetX}Mtr", name = "TES z")
+samplex = PrettyMotorFMBO("XF:07ID1-BI{UCAL-Ax:SampX}Mtr", name="Sample X")
+sampley = PrettyMotorFMBO("XF:07ID1-BI{UCAL-Ax:SampY}Mtr", name="Sample Y")
+samplez = PrettyMotorFMBO("XF:07ID1-BI{UCAL-Ax:SampZ}Mtr", name="Sample Z")
+sampler = PrettyMotorFMBO("XF:07ID1-BI{UCAL-Ax:SampR}Mtr", name="Sample R")
+
+meshz = PrettyMotorFMBO("XF:07ID1-BI{I0Up-Ax:MMesh}Mtr")
+
+
