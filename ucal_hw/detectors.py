1c5a2476872a4057cdfe7cfb34bb1ca42a00f8d4
ctitus <ctitus@bnl.gov>
2021-11-01 17:45:40 -0400

    energy positioner added, motors added, i400 aliases added

--- ucal_hw/detectors.py
+++ ucal_hw/detectors.py
@@ -1,3 +1,15 @@
 from bl_base.detectors import I400
+from sst_tes.readable_tes import TES
 
 i400 = I400("XF:07ID-BI{DM2:I400-1}", name="ucal I400-1")
+tes = TES("tes", address="10.66.51.41", port="4000")
+
+# i400 aliases
+ref = i400.i1
+ref.name = "ref"
+m4drain = i400.i2
+m4drain.name = "m4drain"
+sc = i400.i3
+sc.name = "sc"
+
+thresholds = {"sc": 2e-11}
---
+++ ucal_hw/energy.py
@@ -0,0 +1,4 @@
+from sst_hw.energy import EnPos
+from ucal_hw.motors import sampler
+
+en = EnPos("", rotation_motor=sampler, name="en")
