06fcf850a2d66815b058d2af8b8c5d983a90b2ad
Jamie Titus <ctitus@stanford.edu>
2021-12-05 15:44:07 -0500

    minor changes to manipulator and detectors

--- ucal_hw/manipulator.py
+++ ucal_hw/manipulator.py
@@ -4,7 +4,7 @@ from bl_base.sampleholder import SampleHolder
 from ophyd import Component as Cpt
 
 
-
+manip_origin = vec(0, 0, 531)
 
 # manipulator = Manipulator(bar, "MCS8:PREFIX:PLACEHOLDER:", name="Manipulator")
 
