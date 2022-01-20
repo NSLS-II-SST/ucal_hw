0c55cb92969e5a6c63f92bb765f6bcb33f4fc8fc
Charles Titus <ctitus@bnl.gov>
2022-01-20 18:14:26 -0500

    Hinting so that manipulator (hopefully) works with BEC

--- ucal_hw/manipulator.py
+++ ucal_hw/manipulator.py
@@ -1,17 +1,18 @@
 from bl_base.manipulator import Manipulator4AxBase
 from bl_base.motors import PrettyMotorFMBO
-from bl_base.sampleholder import SampleHolder
 from bl_funcs.geometry.linalg import vec
 from ophyd import Component as Cpt
 
 
 manip_origin = vec(0, 0, 531)
 
+
 class Manipulator(Manipulator4AxBase):
-    x = Cpt(PrettyMotorFMBO, "SampX}Mtr", name="Manipulator X")
-    y = Cpt(PrettyMotorFMBO, "SampY}Mtr",  name="Manipulator Y")
-    z = Cpt(PrettyMotorFMBO, "SampZ}Mtr",  name="Manipulator Z")
-    r = Cpt(PrettyMotorFMBO, "SampTh}Mtr", name="Manipulator R")
+    x = Cpt(PrettyMotorFMBO, "SampX}Mtr", name="Manipulator X", kind='hinted')
+    y = Cpt(PrettyMotorFMBO, "SampY}Mtr",  name="Manipulator Y", kind='hinted')
+    z = Cpt(PrettyMotorFMBO, "SampZ}Mtr",  name="Manipulator Z", kind='hinted')
+    r = Cpt(PrettyMotorFMBO, "SampTh}Mtr", name="Manipulator R", kind='hinted')
 
-manipulator = Manipulator(None, "XF:07ID1-BI{UCAL-Ax:", origin=manip_origin, name="Manipulator")
 
+manipulator = Manipulator(None, "XF:07ID1-BI{UCAL-Ax:", origin=manip_origin,
+                          name="Manipulator")
