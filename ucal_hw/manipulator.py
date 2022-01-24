bdf70c783ecfdda5855ef1eb1ef184729b858282
Charles Titus <ctitus@bnl.gov>
2022-01-24 10:09:05 -0500

    added comment about multimesh location

--- ucal_hw/manipulator.py
+++ ucal_hw/manipulator.py
@@ -3,7 +3,7 @@ from sst_base.motors import PrettyMotorFMBO
 from sst_funcs.geometry.linalg import vec
 from ophyd import Component as Cpt
 
-
+# Note, multimesh is in sst_hw
 manip_origin = vec(0, 0, 531)
 
 
@@ -12,9 +12,6 @@ class Manipulator(Manipulator4AxBase):
     y = Cpt(PrettyMotorFMBO, "SampY}Mtr",  name="Manipulator Y", kind='hinted')
     z = Cpt(PrettyMotorFMBO, "SampZ}Mtr",  name="Manipulator Z", kind='hinted')
     r = Cpt(PrettyMotorFMBO, "SampTh}Mtr", name="Manipulator R", kind='hinted')
-
-
-# Check actual PV name
 
 
 manipulator = Manipulator(None, "XF:07ID1-BI{UCAL-Ax:", origin=manip_origin,
