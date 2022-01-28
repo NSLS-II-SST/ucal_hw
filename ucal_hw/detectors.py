04e5f65f9f3afa39840e9d562f732f9aeffd2bbf
ctitus <ctitus@bnl.gov>
2022-01-28 16:24:48 -0500

    changes for 20220126 beamtime, most things may work

--- ucal_hw/detectors.py
+++ ucal_hw/detectors.py
@@ -5,7 +5,7 @@ from sst_tes.readable_tes import TES
 dm7_i400 = I400("XF:07ID-BI{DM7:I400-1}", name="DM7_I400")
 ucal_i400 = I400("XF:07ID-BI{DM2:I400-1}", name="ucal_I400")
 m5c_i400 = I400("XF07ID-BI{M5C:I400-1}", name="M5C_I400")
-tes = TES("tes", address="10.66.51.41", port=4000)
+tes = TES("tes", address="10.66.48.41", path="/home/pcuser/data", port=4000)
 
 
 def rename_cpt(cpt, name):
@@ -17,10 +17,17 @@ ref = ucal_i400.i2
 rename_cpt(ref, "ref")
 i1 = m5c_i400.i1
 rename_cpt(i1, "i1")
-i0 = ucal_i400.i1
+i0 = dm7_i400.i2
 rename_cpt(i0, "i0")
 sc = ucal_i400.i3
 rename_cpt(sc, "sc")
+dm7_i400.i1.kind = "omitted"
+dm7_i400.i3.kind = "omitted"
+dm7_i400.i4.kind = "omitted"
+
+ucal_i400.i2.kind = "omitted"
 ucal_i400.i4.kind = "omitted"
 
-thresholds = {"sc": 2e-11, "i0": 2e-11, "ref": 2e-11, "i1": 2e-11}
+thresholds = {sc.name: 2e-11, i0.name: 2e-11, ref.name: 2e-11, i1.name: 2e-11}
+basic_dets = [ref, i0, sc]
+det_devices = [ucal_i400, dm7_i400]
