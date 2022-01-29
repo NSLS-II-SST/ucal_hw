54fffe277054563297204d089eb364fba4e57724
Charles Titus <ctitus@bnl.gov>
2022-01-29 13:07:45 -0500

    testing new deadband motor

--- ucal_hw/manipulator.py
+++ ucal_hw/manipulator.py
@@ -1,5 +1,5 @@
 from sst_base.manipulator import Manipulator4AxBase, Manipulator1AxBase
-from sst_base.motors import PrettyMotorFMBO
+from sst_base.motors import PrettyMotorFMBO, DeadbandFMBOEpicsMotor
 from sst_funcs.geometry.linalg import vec
 from ophyd import Component as Cpt
 
@@ -8,10 +8,10 @@ manip_origin = vec(0, 0, 531, 0)
 
 
 class Manipulator(Manipulator4AxBase):
-    x = Cpt(PrettyMotorFMBO, "SampX}Mtr", name="Manipulator X", kind='hinted')
-    y = Cpt(PrettyMotorFMBO, "SampY}Mtr",  name="Manipulator Y", kind='hinted')
-    z = Cpt(PrettyMotorFMBO, "SampZ}Mtr",  name="Manipulator Z", kind='hinted')
-    r = Cpt(PrettyMotorFMBO, "SampTh}Mtr", name="Manipulator R", kind='hinted')
+    x = Cpt(DeadbandFMBOEpicsMotor, "SampX}Mtr", name="Manipulator X", kind='hinted')
+    y = Cpt(DeadbandFMBOEpicsMotor, "SampY}Mtr",  name="Manipulator Y", kind='hinted')
+    z = Cpt(DeadbandFMBOEpicsMotor, "SampZ}Mtr",  name="Manipulator Z", kind='hinted')
+    r = Cpt(DeadbandFMBOEpicsMotor, "SampTh}Mtr", name="Manipulator R", kind='hinted')
 
 
 manipulator = Manipulator(None, "XF:07ID1-BI{UCAL-Ax:", origin=manip_origin,
