7124b028095a6fe88d761fa2b7154c3b2396d122
ctitus <ctitus@bnl.gov>
2023-03-13 20:20:39 -0400

    Epics ADR used rather than pure Ophyd ADR, detectors use ADC now, not I400s, and grating change functions vendored from RSoXS

--- ucal_hw/detectors.py
+++ ucal_hw/detectors.py
@@ -32,14 +32,21 @@ dm7_i400.i4.kind = "omitted"
 #ucal_i400.i2.kind = "omitted"
 ucal_i400.i4.kind = "omitted"
 """
-i0 = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC1_MON", name="ucal_i400_i0up")
+#i0 = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC1_MON", name="ucal_i400_i0up")
+i0 = I400SingleCh("XF:07ID-BI[ADC:1-Ch:7]Volt", name="ucal_i0up")
+i0.rescale.set(-1)
 i0.mean.name = i0.name
-ref = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC2_MON", name="ucal_i400_ref")
+#ref = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC2_MON", name="ucal_i400_ref")
+ref = I400SingleCh("XF:07ID-BI[ADC:1-Ch:5]Volt", name="ucal_ref")
+ref.rescale.set(-1)
 ref.mean.name = ref.name
-sc = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC3_MON", name="ucal_i400_sc")
+#sc = I400SingleCh("XF:07ID-BI{DM2:I400-1}:IC3_MON", name="ucal_i400_sc")
+sc = I400SingleCh("XF:07ID-BI[ADC:1-Ch:4]Volt", name="ucal_sc")
+sc.rescale.set(-1)
 sc.mean.name = sc.name
 i0mir = I400SingleCh("XF:07ID-BI{DM7:I400-1}:IC3_MON", name="DM7_i400_i0mirror")
 i0mir.mean.name = i0mir.name
-thresholds = {sc.name: 2e-11, i0mir.name: 2e-11, ref.name: 2e-11, i0.name: 2e-11}
+#thresholds = {sc.name: 2e-11, i0mir.name: 2e-11, ref.name: 2e-11, i0.name: 2e-11}
+thresholds = {sc.name: .1, i0mir.name: 2e-11, ref.name: .1, i0.name: .1}
 basic_dets = [ref, i0, sc]
 #det_devices = [ucal_i400, dm7_i400]
--- ucal_hw/energy.py
+++ ucal_hw/energy.py
@@ -1,5 +1,48 @@
 from sst_hw.energy import EnPos
 from ucal_hw.manipulator import manipulator
+import bluesky.plan_stubs as bps
+from sst_hw.shutters import psh4
+
 
 en = EnPos("", rotation_motor=manipulator.r, name="en")
 en.scanlock.set(1)
+
+
+def base_grating_to_250():
+    mono_en = en.monoen
+    type = mono_en.gratingx.readback.get()
+    if '250l/mm' in type:
+        print("the grating is already at 250 l/mm")
+        return 0  # the grating is already here
+    print("Moving the grating to 250 l/mm.  This will take a minute...")
+    yield from psh4.close()
+    yield from bps.abs_set(mono_en.gratingx, 2, wait=True)
+    #yield from bps.sleep(60)
+    #yield from bps.mv(mirror2.user_offset, 0.04) #0.0315)
+    #yield from bps.mv(grating.user_offset, -0.0874)#-0.0959)
+    yield from bps.mv(en.m3offset,7.55)
+    yield from bps.mv(mono_en.cff, 1.585)
+    yield from bps.mv(en, 270)
+    yield from psh4.open()
+    print("the grating is now at 250 l/mm signifigant higher order")
+    return 1
+
+
+def base_grating_to_1200():
+    mono_en = en.monoen
+    type = mono_en.gratingx.readback.get()
+    if '1200' in type:
+        print("the grating is already at 1200 l/mm")
+        return 0  # the grating is already here
+    print("Moving the grating to 1200 l/mm.  This will take a minute...")
+    yield from psh4.close()
+    yield from bps.abs_set(mono_en.gratingx, 9, wait=True)
+    #yield from bps.sleep(60)
+    #yield from bps.mv(mirror2.user_offset, 0.2044) #0.1962) #0.2052) # 0.1745)  # 8.1264)
+    #yield from bps.mv(grating.user_offset, 0.0769) #0.0687) # 0.0777) # 0.047)  # 7.2964)  # 7.2948)#7.2956
+    yield from bps.mv(mono_en.cff, 2.05)
+    yield from bps.mv(en.m3offset,7.66)
+    yield from bps.mv(en, 270)
+    yield from psh4.open()
+    print("the grating is now at 1200 l/mm")
+    return 1
